5e8572e858e960cefc2013cdeeb3127a463ac1a1
Hisham Salameh <salamehh@msu.edu>
2022-08-25 16:44:51 -0500

    feat: import and setup pinia store

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
+app.use(router).mount("#app");
