e180bb28595681d4f5511c63436b27f039712e23
Hisham Salameh <salamehh@msu.edu>
2022-08-25 16:07:54 -0500

    chore: project setup complete

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,11 @@
 import NavBar from "@/components/Nav-bar.vue";
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+html,
+body {
+  height: 100%;
+  margin: 0;
+  background-color: #333533;
+}
+</style>
--- src/components/Nav-bar.vue
+++ src/components/Nav-bar.vue
@@ -1,9 +1,40 @@
 <template>
-  <div class="nav-container">
-    <h1>Hello World</h1>
+  <div class="nav">
+    <div class="content-container">
+      <h1>Hisham Salameh</h1>
+      <nav class="nav-items-container">
+        <li>About Me</li>
+        <li>Work Experience</li>
+        <li>Projects</li>
+      </nav>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.nav {
+  width: 100%;
+  background-color: #333533;
+  .content-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 50px;
+    margin: 0 2rem;
+    color: white;
+    h1 {
+      font-family: "Source Code Pro";
+      margin: 0;
+    }
+    .nav-items-container {
+      display: flex;
+      li {
+        list-style-type: none;
+        margin: 0 1rem;
+      }
+    }
+  }
+}
+</style>
