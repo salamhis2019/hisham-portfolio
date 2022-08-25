49c47f31fcce8751030478130f9626e3b6f6297b
Hisham Salameh <salamehh@msu.edu>
2022-08-25 16:48:07 -0500

    refactor: change html structure to make it more readable

--- src/components/Nav-bar.vue
+++ src/components/Nav-bar.vue
@@ -3,9 +3,15 @@
     <div class="content-container">
       <h1>Hisham Salameh</h1>
       <nav class="nav-items-container">
-        <li><a href="">Work Experience</a></li>
-        <li><a href="">Projects</a></li>
-        <li><a href="">About Me</a></li>
+        <li>
+          <a href="">Work Experience</a>
+        </li>
+        <li>
+          <a href="">Projects</a>
+        </li>
+        <li>
+          <a href="">About Me</a>
+        </li>
       </nav>
     </div>
   </div>
