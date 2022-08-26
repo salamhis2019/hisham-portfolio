b2c2aca1c3d3a29d9dc3e5bf66beb6612becfd32
Hisham Salameh <salamehh@msu.edu>
2022-08-26 01:25:54 -0500

    feat: complete button component

--- src/components/button-links.vue
+++ src/components/button-links.vue
@@ -2,8 +2,10 @@
   <a 
     :href="link"
     target="blank"
+    class="button-link"
   >
     {{ text }}
+    <i class='fas fa-external-link-alt' style='font-size:24px'></i>
   </a>
 </template>
 
@@ -13,4 +15,20 @@ import { defineProps } from 'vue';
 defineProps(['text', 'link'])
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.button-link {
+  padding: 0.5rem 1rem;
+  font-size: 24px;
+  font-family: 'Source Code Pro';
+  text-decoration: none;
+  color: white;
+  border: 1px solid white;
+  border-radius: 12px;
+  background: rgba(255, 255, 255, 0.123);
+  box-shadow: 2px 2px 0px white;
+  transition: 0.2s ease;
+  &:hover {
+    background: rgba(255, 255, 255, 0.27);
+  }
+}
+</style>
