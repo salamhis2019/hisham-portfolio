585be9eb8edb88aa5b06efffa85b41de663bf6ca
Hisham Salameh <salamehh@msu.edu>
2022-08-25 22:17:18 -0500

    feat: create main view for project

---
+++ src/views/LandingView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="parent-container">
+    <NavBar />
+    <component :is="currentComponent" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { storeToRefs } from "pinia";
+import NavBar from "@/components/Nav-bar.vue";
+import usePortfolioStore from "@/stores/portfolio.store";
+
+const portfolioStore = usePortfolioStore();
+
+const { currentComponent } = storeToRefs(portfolioStore);
+</script>
+
+<style lang="scss"></style>
