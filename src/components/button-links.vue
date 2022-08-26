79db1f10b465d173e02762b709a7a64f7652f738
Hisham Salameh <salamehh@msu.edu>
2022-08-26 01:17:22 -0500

    feat: create button links component

---
+++ src/components/button-links.vue
@@ -0,0 +1,16 @@
+<template>
+  <a 
+    :href="link"
+    target="blank"
+  >
+    {{ text }}
+  </a>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+defineProps(['text', 'link'])
+</script>
+
+<style lang="scss" scoped></style>
