3a524c7a25343e705a25af49dc08860aefdb79ff
Hisham Salameh <salamehh@msu.edu>
2022-08-26 00:16:31 -0500

    feat: add content to project section container

--- src/components/projects-section.vue
+++ src/components/projects-section.vue
@@ -1,9 +1,40 @@
 <template>
   <div class="work-experience-container">
-    <h1>Projects</h1>
+    <div class="work-experience-content">
+      <div class="header-container">
+        <h2 class="section-header">
+          Projects
+        </h2>
+      </div>
+      <div class="content-container">
+        <img class="stock-icon" src="@/assets/stock-icon.png" alt="">
+      </div>      
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.work-experience-container {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  .section-header {
+    font-size: 4rem;
+    font-family: 'Source Code Pro';
+    color: white;
+    .period {
+      height: 20px;
+      font-size: 7rem;
+      font-family: 'Oswald';
+    }
+  }
+  .content-container {
+    width: 600px;
+    .stock-icon {
+      width: 15rem;
+    }
+  }
+}
+</style>
