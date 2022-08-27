c0329e54b39db487f1a970a2f9ab1f1f66e81ba9
Hisham Salameh <salamehh@msu.edu>
2022-08-26 22:07:54 -0500

    feat: create footer component

---
+++ src/components/footer-section.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="footer-parent-container">
+    <ul class="content-container">
+      <li>
+        <i class="fa fa-linkedin"></i>
+      </li>
+      <li>
+        <i class="fa fa-github"></i>      
+      </li>
+    </ul>
+    <p class="contact">Contact me at at salamehh@msu.edu</p>
+    <p class="created-by">Created By <span class="name">Hisham Salameh</span></p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.footer-parent-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  min-height: 150px;
+  padding: 1.5rem 0;
+  background: #333533;
+  border-top: 1px solid white;
+  .content-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    li {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 45px;
+      width: 45px;
+      margin: 0 1rem;
+      padding: 1rem;
+      font-size: 48px;
+      border-radius: 50%;
+      color: black;
+      background: white;
+      list-style-type: none;
+    }
+  }
+  .contact {
+    margin: 0;
+    margin-top: 1.5rem;
+    font-family: 'Source Sans Pro';
+    color: white;
+    font-size: 1.2rem;
+  }
+  .created-by {
+    margin: 0;
+    margin-top: 1.5rem;
+    font-family: 'Source Sans Pro';
+    color: rgba(255, 255, 255, 0.595);
+    font-size: 1.2rem;
+    .name {
+      font-style: italic;
+      font-size: 1.5rem;
+      font-weight: 700;
+      background: -webkit-linear-gradient(#0088ff, #ff00bb);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
+  }
+}
+</style>
