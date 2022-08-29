6251d60abd28accf4b2fa886337082840a186c48
Hisham Salameh <salamehh@msu.edu>
2022-08-29 00:22:06 -0400

    feat: create setTimeout to load data after 2.2 seconds

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="parent-container">
-    <NavBar />
-    <div class="page-container">
-      <Transition>
-        <component :is="currentComponent" />
-      </Transition>
+  <div class="landing-view-container">
+    <div
+      v-if="contentReady"
+      class="portfolio-content-container"
+    >
+      <NavBar />
+      <div class="page-container">
+        <Transition>
+          <component :is="currentComponent" />
+        </Transition>
+      </div>
+      <Footer />
     </div>
   </div>
-  <Footer />
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue";
 import { storeToRefs } from "pinia";
+import LoadingPage from "@/components/loading-page.vue"
 import NavBar from "@/components/Nav-bar.vue";
 import Footer from "@/components/footer-section.vue";
 import usePortfolioStore from "@/stores/portfolio.store";
@@ -19,11 +26,19 @@ import usePortfolioStore from "@/stores/portfolio.store";
 const portfolioStore = usePortfolioStore();
 
 const { currentComponent } = storeToRefs(portfolioStore);
+
+const contentReady = ref<boolean>(false);
+
+setTimeout(() => {
+  contentReady.value = true;
+}, 2200)
+
+
 </script>
 
 <style lang="scss">
-.parent-container {
-  min-height: 100%;
+.landing-view-container {
+  height: 100%;
   .page-container {
     height: 100%;
     background-color: #1c202b;
