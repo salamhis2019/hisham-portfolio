b6eb814bcfcd5cff6b8585066791eb078a1dba4d
Hisham Salameh <salamehh@msu.edu>
2022-08-29 21:46:38 -0400

    feat: update home page content and styles

--- src/components/home-page.vue
+++ src/components/home-page.vue
@@ -1,98 +1,106 @@
 <template>
-  <div class="work-experience-container">
-    <div class="work-experience-content">
-      <div class="header-container">
-        <div class="header-content">
+  <div class="homepage-container">
+    <div class="homepage-content">
+      <div class="hero-content">
+        <div class="home-description">
           <h2 class="section-header">
-            Work Experience<span class="period"> .</span>
+            Hisham Salameh<span class="period"> .</span>
           </h2>
-          <p class="description">Here are all of my most recent experiences with web development and what I have learned.</p>          
+          <p class="description">UI Engineer and UX Designer with a focus on creating modern web applications</p>
+          <ul class="content-container">
+            <li>
+              <i class="fa fa-linkedin"></i>
+            </li>
+            <li>
+              <i class="fa fa-github"></i>      
+            </li>
+          </ul>
         </div>
+        <img
+          class="my-image"
+          src="@/assets/homepage/headshot.png"
+          alt="about me image"
+        >
       </div>
-      <hr>
     </div>
+    <hr>
   </div>
 </template>
 
-<script lang="ts" setup></script>
-
 <style lang="scss" scoped>
-.work-experience-container {
+.homepage-container {
   display: flex;
   justify-content: center;
+  flex-direction: column;
   width: 100%;
   background: #1c202b;
-  .work-experience-content {
-    width: 900px;
-    .header-container {
+  .homepage-content {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    padding: 5rem;
+    width: 100%;
+    .hero-content {
       display: flex;
+      justify-content: center;
       align-items: center;
-      justify-content: left;
-      height: 300px;
-      padding-bottom: 2rem;
-      .description {
-        font-family: 'Source Sans Pro';
-        font-size: 1.2rem;
-        color: white;
+      gap: 3rem;
+      width: 100%;
+      .my-image {
+        width: 20rem;
+        background: linear-gradient(to right, #004cff7e, #ff00807d);
+        border-radius: 50%;
+        border-bottom: 3px dashed white;
+        border-right: 3px dashed white;
+        border-left: 3px dashed white;
+        border-top: 3px solid white;
       }
-      .section-header {
-        margin-bottom: 3rem;
-        font-size: 4rem;
-        font-family: 'Source Code Pro';
-        color: white;
-        .period {
-          font-size: 5rem;
-          font-family: 'Oswald';
+      .home-description {
+        max-width: 650px;
+        font-family: 'Source Sans Pro';
+        .section-header {
+          font-family: 'Source Code Pro';
+          font-size: 4rem;
+          color: white;
+        }
+        .description {
+          color: white;
+          font-size: 2rem;
+          font-weight: 600;
           background: -webkit-linear-gradient(#004cff, #ff0080);
           -webkit-background-clip: text;
           -webkit-text-fill-color: transparent;
         }
-      }
-    }
-    hr {
-      border: 0;
-      height: 2.5px;
-      background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(255, 255, 255, 0.75), rgba(0, 0, 0, 0));
-      margin-bottom: 4rem;
-    }
-  }
-  .content-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 110%;
-    min-height: 300px;
-    margin-bottom: 3rem;
-    .stock-icon {
-      width: 240px;
-      height: 240px;
-      transition: 0.2s ease;
-      &:hover {
-        transform: scale(1.1);
-      }
-    }
-    .card-container {
-      height: 100%;
-      width: 650px;
-      h3 {
-        margin: 0;
-        color: white;
-        font-family: 'Source Sans Pro';
-        font-size: 2rem;
-      }
-      .description-container {
-        padding: 0;
-        margin-bottom: 2rem;
-        font-size: 1.2rem;
-        font-family: 'Source Sans Pro';
-        font-weight: 600;
-        color: rgba(255, 255, 255, 0.739);
-        li {
+        .content-container {
+          display: flex;
+          align-items: center;
+          gap: 3rem;
+          padding: 0;
           list-style-type: none;
-          margin-bottom: 1.2rem;
+          .fa-linkedin {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: #1c202b;
+            background: white;
+            font-size: 3rem;
+            height: 69px;
+            width: 69px;
+            border-radius: 50%;
+          }
+          .fa-github {
+            color: white;
+            font-size: 5rem;
+          }
         }
       }
     }
+  }
+  hr {
+    border: 0;
+    height: 2.5px;
+    background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(255, 255, 255, 0.75), rgba(0, 0, 0, 0));
+    margin-bottom: 4rem;
   }
 }
 </style>
