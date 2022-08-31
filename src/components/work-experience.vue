6d9f9d3dd0d03dc98081b1fb4c9e2f32a82d18c8
Hisham Salameh <salamehh@msu.edu>
2022-08-31 19:30:52 -0400

    feat: allow template to iterate through imported object

--- src/components/work-experience.vue
+++ src/components/work-experience.vue
@@ -1,48 +1,52 @@
 <template>
-  <div class="experience-container">
-    <div class="job-overview">
-      <p class="logo">{{ logo }}</p>
-    </div>
+  <div
+    v-for="workplace in workExperience"
+    :key="workplace.company"
+    class="experience-container">
     <div class="job-description">
-      <div class="title-container">
-        <p class="title">{{ companyTitle }} - {{ jobTitle }}</p>
-        <p class="date">{{ date }}</p>
+      <p class="title company-title">
+        {{ workplace.company }}
+      </p>
+      <div
+        v-for="{ title, date, responsibilities } in workplace.positions"
+        :key="title"
+        class="positions-container"
+      >
+        <div class="title-container">
+          <p class="job-title">{{ title }}</p>
+          <p class="date">{{ date }}</p>
+        </div>
+        <ul
+          v-for="description in responsibilities"
+          :key="description"
+        >
+          <li>
+            {{ description }}
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          {{ bulletOne }}
-        </li>
-        <li>
-          {{ bulletTwo }}
-        </li>
-        <li>
-          {{ bulletThree }}
-        </li>
-      </ul>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from 'vue';
+import Jobs from "@/json/jobs.js";
 
-defineProps([
-  "companyTitle",
-  "logo",
-  "jobTitle",
-  "date",
-  "bulletOne",
-  "bulletTwo",
-  "bulletThree"
-])
+const workExperience = Jobs
 </script>
 
 <style lang="scss" scoped>
 .experience-container {
-display: flex;
-gap: 2rem;
-font-family: 'Source Sans Pro';
-color: white;
+  display: flex;
+  width: 700px;
+  margin-bottom: 2rem;
+  padding: 2rem;
+  gap: 3rem;
+  font-family: 'Source Sans Pro';
+  color: white;
+  border-radius: 15px;
+  background: linear-gradient(to right, #333533, #3f403f);
+  box-shadow: 6px 6px 0px white;
   .job-description, .job-overview {
     h3 {
       margin: 0;
@@ -56,20 +60,26 @@ color: white;
       font-size: 2rem;
     }
     .logo {
-      font-size: 7rem;
+      font-size: 8rem;
     }
   }
   .job-description {
     display: flex;
     flex-direction: column;
     gap: 1rem;
+    .company-title {
+      color: white;
+      font-size: 2rem;
+      font-weight: 700;
+      text-shadow: 3px 3px 0px #ff0080b7;
+    }
     .title-container {
       display: flex;
       justify-content: space-between;
       align-items: center;
       width: 100%;
       .title {
-        font-size: 2rem;
+        font-size: 1.5rem;
         font-weight: 500;
       }
       .date {
@@ -77,15 +87,19 @@ color: white;
         font-style: italic;
       }
     }
+    .job-title {
+      font-size: 1.5rem;
+    }
     ul {
       display: flex;
       flex-direction: column;
-      gap: 0.5rem;
+      gap: 1rem;
       padding: 0;
       margin: 0;
       list-style-type: none;
       li {
         font-size: 1.2rem;
+        font-weight: 300;
       }
     }
   }
