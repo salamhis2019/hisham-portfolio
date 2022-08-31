e8b64be25f885408cc745a445d932358b2e752fb
Hisham Salameh <salamehh@msu.edu>
2022-08-31 19:29:24 -0400

    feat: create JSON file containing all of the job items

---
+++ src/json/jobs.js
@@ -0,0 +1,63 @@
+export const test = [
+  {
+    company: 'Apple',
+    positions: [
+      {
+        title: 'UI Engineering Intern',
+        date: '05/31/2022 - 08/26/2022',
+        responsibilities: [
+          'Own and drive UI assigned tasks for all phases from development to production',
+          'Become subject matter expert for intern project while working with technical leaders',
+          'Adhere to various processes defined within team such as Agile, code review, or Git.',
+        ]
+      },
+      {
+        title: 'iOS T1 Advisor',
+        date: '05/25/2021 - Currrent',
+        responsibilities: [
+          'Provide exemplary professional customer service and technical support on Apple products and software while under pressure',
+          'Assembled team to assist manager in optimizing advisor role and improve mental and physical well- being on the job (project in progress)',
+          'Consistently performed highly with customer satisfaction and resolving issues with efficiency'
+        ]
+      }
+    ]
+  },
+  {
+    company: 'Michigan State University',
+    positions: [
+      {
+        title: 'CAS 204 Web Design Teaching Assistant',
+        date: '10/01/2021 - Current',
+        responsibilities: [
+          'Met one on one with students to guide them through understanding the fundamentals of website creation with HTML and CSS',
+          'Graded student assignments and provided additional feedback on how to improve their projects and website format'
+        ]
+      },
+      {
+        title: 'Advertising and Public Relations Web Developer',
+        date: '10/01/2021 - 01/31/2022',
+        responsibilities: [
+          'Created prototypes and wireframes to present to project manager to determine which approach best fits the needs of the project',
+          'Created interface elements using JavaScript to keep students engaged while maintaining a well formatted HTML structure',
+          'Created an image carousel with bootstrap and manipulated DOM elements throug JavaScript to create logic for the interface',
+        ]
+      }
+    ]
+  },
+  {
+    company: 'Essexville Hampton Public Schools',
+    positions: [
+      {
+        title: 'Web Developer/Tech Aide',
+        date: '02/14/2021 - 08/31/2021',
+        responsibilities: [
+          'Implemented a focused, user-friendly interface with new HTML and CSS elements',
+          'Implemented scheduling and news solution by embedding external documents into the website to allow for updateable info, saving the school $5000 annually',
+          'Collaborated with peers and set meetings with departments and leadership across the school district to ensure each area was well represented'
+        ]
+      }
+    ]
+  }
+]
+
+export default test;
