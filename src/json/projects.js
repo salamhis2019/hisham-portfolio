4e7c58c3f8cd0ecf8d89d18d1749694083dc0958
Hisham Salameh <salamehh@msu.edu>
2022-09-08 10:57:17 -0400

    feat: update JSON for projects.js

--- src/json/projects.js
+++ src/json/projects.js
@@ -1,75 +1,22 @@
-export const test = [
+export const projects = [
   {
-    company: 'Apple',
-    positions: [
-      {
-        title: 'UI Engineering Intern',
-        date: '05/31/2022 - 08/26/2022',
-        responsibilities: [
-          'Own and drive UI assigned tasks for all phases from development to production',
-          'Become subject matter expert for intern project while working with technical leaders',
-          'Adhere to various processes defined within team such as Agile, code review, or Git.',
-        ],
-        skills: [
-          'Vue', 'JavaScript', 'SASS', 'HTML', 'Jest', 'Vue Test Utils', 'Sketch', 'Photoshop'
-        ]
-      },
-      {
-        title: 'iOS T1 Advisor',
-        date: '05/25/2021 - Currrent',
-        responsibilities: [
-          'Provide exemplary professional customer service and technical support on Apple products and software while under pressure',
-          'Assembled team to assist manager in optimizing advisor role and improve mental and physical well- being on the job (project in progress)',
-          'Consistently performed highly with customer satisfaction and resolving issues with efficiency'
-        ],
-      }
+    title: 'Stock Project with Vantage API',
+    description: [
+      'Developed a stock app that displays recent closing data from various stocks/cryptos from a public API.',
+      'The UI was created using Vue 3 and SASS, and the data was fetched using axios and the fetch API.',
+      'Utilized the most recent vue features and rendered the components dynamically between the two apps. Reusability concepts were implemented for readability and the chart.js library was used to render the stock chart.',
+      'Used Pinia to maintain the state of the app and utilized the composition API.'
     ]
   },
   {
-    company: 'Michigan State University',
-    positions: [
-      {
-        title: 'CAS 204 Web Design Teaching Assistant',
-        date: '10/01/2021 - Current',
-        responsibilities: [
-          'Met one on one with students to guide them through understanding the fundamentals of website creation with HTML and CSS',
-          'Graded student assignments and provided additional feedback on how to improve their projects and website format'
-        ],
-        skills: [
-          'HTML', 'CSS',
-        ]
-      },
-      {
-        title: 'Advertising and Public Relations Web Developer',
-        date: '10/01/2021 - 01/31/2022',
-        responsibilities: [
-          'Created prototypes and wireframes to present to project manager to determine which approach best fits the needs of the project',
-          'Created interface elements using JavaScript to keep students engaged while maintaining a well formatted HTML structure',
-          'Created an image carousel with bootstrap and manipulated DOM elements throug JavaScript to create logic for the interface',
-        ],
-        skills: [
-          'JavaScript', 'CSS', 'HTML', 'Photoshop', 'Figma'
-        ]
-      }
+    title: 'Portfolio Project with Vue',
+    description: [
+      'Developed a personal portfolio site using Vue 3 and the script setup with Vite, Pinia and SASS.',
+      'Tested the application using vue test utils and jest, includes unit and store testing.',
+      'Utilized the most recent vue features and created the portfolio by making it route driven',
+      'Implemented good github hygeine by creating semantic commits and working on different branches for features',
     ]
   },
-  {
-    company: 'Essexville Hampton Public Schools',
-    positions: [
-      {
-        title: 'Web Developer/Tech Aide',
-        date: '02/14/2021 - 08/31/2021',
-        responsibilities: [
-          'Implemented a focused, user-friendly interface with new HTML and CSS elements',
-          'Implemented scheduling and news solution by embedding external documents into the website to allow for updateable info, saving the school $5000 annually',
-          'Collaborated with peers and set meetings with departments and leadership across the school district to ensure each area was well represented'
-        ], 
-        skills: [
-          'HTML', 'SCSS', 'Wordpress', 'Photoshop'
-        ]
-      }
-    ]
-  }
 ]
 
-export default test;
+export default projects;
