79423d6cc390cf33de8893897599e4edd79c8698
Hisham Salameh <salamehh@msu.edu>
2022-10-19 11:02:11 -0400

    feat: add more description to each project

--- src/json/projects.js
+++ src/json/projects.js
@@ -2,10 +2,10 @@ export const projects = [
   {
     title: 'Stock Project with Vantage API',
     description: [
-      'Developed a stock app that displays recent closing data from various stocks/cryptos from a public API.',
-      'The UI was created using Vue 3 and SASS, and the data was fetched using axios and the fetch API.',
-      'Utilized the most recent vue features and rendered the components dynamically between the two apps. Reusability concepts were implemented for readability and the chart.js library was used to render the stock chart.',
-      'Used Pinia to maintain the state of the app and utilized the composition API.'
+      'Developed a stock app that displays recent closing, opening, price, and volume data on various stocks and cryptos. A public API called the vantage finance API was used to create this.',
+      'The UI was created using Vue 3 and SASS, and the data from the vantage API was fetched using axios and the fetch API.',
+      'The most recent Vue.js features were used while rendering the components dynamically between the crypto and stock components. Reusability concepts were in the forefront of the creation of the stock app and were implemented to improve readability. To create the stock chart, chart.js was used, which is a free open public library.',
+      'Pinia was used to maintain the state of the app. Towards the beginning of the development phase of the app the options API was used, however towards the end there were some components that were refactored to utilize the composition API and Vue.js most recent features.'
     ],
     link: 'https://vuestockapp.firebaseapp.com/',
     skills: ['Vue.js', 'JavaScript', 'HTML', 'CSS', 'Vue Router', 'chart.js']
@@ -13,10 +13,10 @@ export const projects = [
   {
     title: 'Portfolio Project with Vue',
     description: [
-      'Developed a personal portfolio site using Vue 3 and the script setup with Vite, Pinia and SASS.',
-      'Tested the application using vue test utils and jest, includes unit and store testing.',
-      'Utilized the most recent vue features and created the portfolio by making it route driven',
-      'Implemented good github hygeine by creating semantic commits and working on different branches for features',
+      'Developed a personal portfolio site to display my recent work experiences and projects using Vue 3 and the script setup with Vite, Pinia and SASS.',
+      'Tested the application using Vue.js test utils and jest, includes unit and store testing.',
+      'Utilized the most recent vue features and created the portfolio by making it route driven.',
+      'Implemented github hygeine by creating semantic commits and working on different branches for features',
     ],
     link: 'https://github.com/salamhis2019/hisham-portfolio',
     skills: ['Vue.js', 'TypeScript', 'JavaScript', 'HTML', 'SASS', 'jest.js', 'UX Design', 'Acessibility', 'Reusability', 'Verison Control']
