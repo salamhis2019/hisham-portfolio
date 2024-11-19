74eec3e50f804e19678824e7cdb16ac730b62192
Hisham Salameh <hsalameh@apple.com>
2024-11-19 14:25:04 -0600

    refactor: remove unused transitions

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,10 +1,6 @@
 <template>
-  <div class="landing-view-container">
-    <Transition>
-      <div class="portfolio-content-container">
-        <component :is="PortfolioContent" />
-      </div>
-    </Transition>
+  <div class="landing-view">
+    <portfolio-content />
   </div>
 </template>
 
@@ -13,26 +9,7 @@ import PortfolioContent from "@/components/portfolio-content.vue";
 </script>
 
 <style lang="scss" scoped>
-.landing-view-container {
-  height: 100%;
-  .portfolio-content-container {
-    height: calc(100% - 200px);
-  }
-}
-
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
-.v-enter-active {
-  transition: opacity 1.5s ease-in;
-}
-
-.leave-from {
-  opacity: 1;
-}
-
-.v-leave-active {
-  transition: opacity 1s ease-out;
+.landing-view {
+  height: calc(100% - 200px);
 }
 </style>
