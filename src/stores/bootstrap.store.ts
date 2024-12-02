9b6cd0bed1caea46cae4e0ae42f9583a004ef5f0
Hisham Salameh <hsalameh@apple.com>
2024-12-02 16:26:47 -0600

    refactor: create pinia method to fetch projects

--- src/stores/bootstrap.store.ts
+++ src/stores/bootstrap.store.ts
@@ -1,8 +1,9 @@
 import { ref, type Ref } from "vue";
 import { defineStore } from "pinia";
 import WorkExperiencesMock from '../json/jobs.mocks';
-
+import ProjectsMock from '../json/projects.mocks';
 import type { WorkExperience } from "@/types/jobs.types";
+import type { Project } from "@/types/projects.types";
 
 /**
  * Todo: create swift app to integrate with firebase instead of using mock json
@@ -10,26 +11,38 @@ import type { WorkExperience } from "@/types/jobs.types";
 
 interface State {
   workExperiences: Ref<WorkExperience[]>;
+  projects: Ref<Project[]>
   getWorkExperiences: () => void;
+  getProjects: () => void;
 }
 
 export const useBootstrapStore = defineStore("bootstrap", (): State => {
   // State
 
   const workExperiences = ref<WorkExperience[]>([]);
+  const projects = ref<Project[]>([]);
 
   // Methods
   
   // TODO: create async function which fetches from firebase
   function getWorkExperiences(): void {
-    setTimeout(() => {
+    setTimeout((): void => {
       workExperiences.value = WorkExperiencesMock
       console.log(workExperiences.value);
     }, 1500)
   }
 
+  // TODO: create async function to fetch projects
+  function getProjects(): void {
+    setTimeout((): void => {
+      projects.value = ProjectsMock;
+    }, 1500);
+  }
+
   return {
     workExperiences,
-    getWorkExperiences
+    projects,
+    getWorkExperiences,
+    getProjects
   }
 })
