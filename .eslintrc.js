b9048a92fec9d87184b2f3b7828049b7c8d8f03c
Hisham Salameh <hsalameh@apple.com>
2025-08-26 10:51:53 -0500

    feat: add eslint standards

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,114 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
+    es2021: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-strongly-recommended",
     "eslint:recommended",
     "@vue/typescript/recommended",
+    "eslint-config-prettier",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2021,
+    sourceType: "module",
   },
+  plugins: ["vue", "@typescript-eslint", "prettier"],
   rules: {
+    // Console and debugging
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    
+    // Code quality
+    "no-unused-vars": "off", // Handled by TypeScript
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "warn",
+    
+    // Vue specific rules
+    "vue/multi-word-component-names": "off",
+    "vue/component-name-in-template-casing": ["error", "kebab-case"],
+    "vue/component-definition-name-casing": ["error", "PascalCase"],
+    "vue/prop-name-casing": ["error", "camelCase"],
+    "vue/attribute-hyphenation": ["error", "always"],
+    "vue/v-on-event-hyphenation": ["error", "always"],
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": { "max": 3 },
+      "multiline": { "max": 1 }
+    }],
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+        "normal": "always",
+        "component": "always"
+      }
+    }],
+    "vue/order-in-components": ["error", {
+      "order": [
+        "el",
+        "name",
+        "key",
+        "parent",
+        "functional",
+        ["delimiters", "comments"],
+        ["components", "directives", "filters"],
+        "extends",
+        "mixins",
+        ["provide", "inject"],
+        "ROUTER_GUARDS",
+        "layout",
+        "middleware",
+        "validate",
+        "scrollToTop",
+        "transition",
+        "loading",
+        "inheritAttrs",
+        "model",
+        ["props", "propsData"],
+        "emits",
+        "setup",
+        "asyncData",
+        "data",
+        "fetch",
+        "head",
+        "computed",
+        "watch",
+        "watchQuery",
+        "LIFECYCLE_HOOKS",
+        "methods",
+        ["template", "render"],
+        "renderError"
+      ]
+    }],
+    
+    // General JavaScript/TypeScript rules
+    "prefer-const": "error",
+    "no-var": "error",
+    "object-shorthand": "error",
+    "prefer-template": "error",
+    "template-curly-spacing": "error",
+    "arrow-spacing": "error",
+    "comma-dangle": ["error", "always-multiline"],
+    "semi": ["error", "always"],
+    "quotes": ["error", "single", { "avoidEscape": true }],
+    "indent": ["error", 2, { "SwitchCase": 1 }],
+    "no-trailing-spaces": "error",
+    "eol-last": "error",
+    "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
+    
+    // Prettier integration
+    "prettier/prettier": ["error", {
+      "singleQuote": true,
+      "semi": true,
+      "trailingComma": "all",
+      "tabWidth": 2,
+      "printWidth": 100,
+      "endOfLine": "lf"
+    }]
   },
   overrides: [
     {
@@ -24,6 +120,9 @@ module.exports = {
       env: {
         jest: true,
       },
+      rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+      }
     },
   ],
 };
